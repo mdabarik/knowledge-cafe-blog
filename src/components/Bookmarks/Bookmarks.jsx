d9bbc05d261fa5f371b1d07ff2962577db14aa0a
mdabarik <mdabarik19@gmail.com>
2023-09-13 16:49:40 +0600

    key fixed

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -7,7 +7,7 @@ const Bookmarks = ({bookmarks, readingTime}) => {
             <h1 className="text-3xl">bookmarks: {bookmarks.length}</h1>
             <p>Reading Time: {readingTime}</p>
             {
-                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>)
+                bookmarks.map((bookmark, idx) => <Bookmark key={idx} bookmark={bookmark}></Bookmark>)
             }
         </div>
     );
